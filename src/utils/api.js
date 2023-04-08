cce05a450ef8a11f42b29ba0a8e1c589c1050c74
Natali Kamenkova <natalikamenkova@gmail.com>
2023-04-08 20:56:39 +0300

    new url

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,6 +1,6 @@
 //import { config } from "../utils/utils.js"
 export const config = {
-  url: 'https://mesto.nomoreparties.co/v1/cohort-51',
+  url: 'http://natali.nomoredomains.monster',
   headers: {
     authorization: 'c14fd4d2-b83b-4faf-994c-ea33775685d1',
     "Content-Type": "application/json"
